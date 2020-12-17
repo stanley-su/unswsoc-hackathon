6f48505375b63fb34306109639d1ef41722dc45e
Vincent Wong <vincent@vwong.dev>
2020-12-17 19:13:55 +1100

    Update function to insert new hackathon

--- backend/src/models/hackathon.ts
+++ backend/src/models/hackathon.ts
@@ -8,24 +8,21 @@ function getCurrentHackathons() {
   return pool.query("SELECT * FROM hackathon WHERE end_date::date >= NOW()")
 };
 
-async function createNewHackathon(title: string, startDate: Date, endDate: Date, description: string="",) {
+async function createNewHackathon(title: string, startDate: Date, endDate: Date, description: string = "") {
   const client = await pool.connect();
 
-  (async () => {
-    try {
-      await client.query("BEGIN");
-      const insertText = `INSERT INTO hackathon(title, description, start_date, end_date)
-                          VALUES ($1, $2, $3, $4);`;
-      const insertValues = [title, startDate, endDate, description];
-      await client.query(insertText, insertValues);
-      await client.query("COMMIT");
-    } catch (e) {
-      await client.query("ROLLBACK");
-      throw e;
-    } finally {
-      client.release();
-    }
-  })().catch(e => console.error(e.stack));
+  try {
+    await client.query("BEGIN");
+    const insertText = `INSERT INTO hackathon(title, description, start_date, end_date)
+                        VALUES ($1, $2, $3, $4);`;
+    const insertValues = [title, startDate, endDate, description];
+    await client.query(insertText, insertValues);
+    await client.query("COMMIT");
+    client.release();
+  } catch (e) {
+    await client.query("ROLLBACK");
+    throw e;
+  }
 }
 
-export { getAllHackathons, getCurrentHackathons };
+export { getAllHackathons, getCurrentHackathons, createNewHackathon };
