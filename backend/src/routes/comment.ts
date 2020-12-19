24377804c663d2fe26416d3638406e7dc02c459d
Vincent Wong <vincent@vwong.dev>
2020-12-19 21:43:35 +1100

    Add documentation for comment routes

--- backend/src/routes/comment.ts
+++ backend/src/routes/comment.ts
@@ -4,6 +4,6 @@ const router = express.Router();
 import * as controller from "../controllers/comment";
 
 router.get("/", controller.getComments);
-router.post("new", controller.makeComment);
+router.post("/new", controller.makeComment);
 
 export default router;
