80ce0efd35a926516326abb035daaa9db3ba4bc2
Vincent Wong <vincent@vwong.dev>
2020-12-25 00:27:19 +1100

    Add function to add new project to database

--- backend/src/models/project.ts
+++ backend/src/models/project.ts
@@ -10,6 +10,24 @@ function getSingleProject(projectId: string) {
 
 function getProjectsForHackathon(hackathonId: string) {
   return pool.query("SELECT * from project where hackathon_id = $1", [hackathonId]);
-}
+};
+
+async function createNewProject(title: string, startDate: Date, endDate: Date, description: string="") {
+  const client = await pool.connect();
+
+  try {
+    await client.query("BEGIN");
+    const insertText = `INSERT INTO project(title, description, startDate, endDate)
+                        VALUES ($1, $2, $3, $4);`;
+    const insertValues = [title, description, startDate, endDate];
+    await client.query(insertText, insertValues);
+    await client.query("COMMIT");
+    client.release();
+  } catch (e) {
+    await client.query("ROLLBACK");
+    client.release();
+    throw e;
+  };
+};
 
 export { getAllProjects, getSingleProject, getProjectsForHackathon };
