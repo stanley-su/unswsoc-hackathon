d4bcb3cd21f07529148c8a33e277138448332a9a
Vincent Wong <vincent@vwong.dev>
2020-12-30 11:36:27 +1100

    Use leaderboard route in backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import express from "express";
 import projectRouter from "./routes/project";
 import hackathonRouter from "./routes/hackathon";
 import commentRouter from "./routes/comment";
+import personRouter from "./routes/person";
 
 const app = express();
 const port = 4000;
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use("/project", projectRouter);
 app.use("/hackathon", hackathonRouter);
 app.use("/comment", commentRouter);
+app.use("/person", personRouter);
 
 app.get("/", (_, res) => {
   res.send('Test123!');
