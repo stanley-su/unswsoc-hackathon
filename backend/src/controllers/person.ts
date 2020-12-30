1118c7858039da81326e894dc4cd750e9045b419
Vincent Wong <vincent@vwong.dev>
2020-12-30 13:56:47 +1100

    Add backend route for individual querying individual user

--- backend/src/controllers/person.ts
+++ backend/src/controllers/person.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import * as model from "../models/person";
 
+async function getPerson(req: express.Request, res: express.Response) {
+  const queryResult = await model.getPerson((req.query as any).id);
+  res.send(queryResult.rows);
+};
+
 async function getLeaderBoard(_: express.Request, res: express.Response) {
   const queryResult = await model.getLeaderBoard();
   res.send(queryResult.rows);
 };
 
-export { getLeaderBoard };
+export { getLeaderBoard, getPerson };
