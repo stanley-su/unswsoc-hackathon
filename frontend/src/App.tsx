733dada7c6d3ebf8addd4d6eb4cce5c814be9169
Vincent Wong <vincent@vwong.dev>
2020-12-31 12:03:01 +1100

    Resolve frontend conflicts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
-=======
-import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
->>>>>>> e56f08ac62caef2ef9b6d843ad5e7c16baf07060
 
 import NavBar from "./navbar/NavBar";
 import Home from "./home/Home";
@@ -13,16 +9,11 @@ import User from "./user/User";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <>
-      <Feature text={result} />
+    <BrowserRouter>
+      <NavBar />
       <a href="https://github.com/login/oauth/authorize?client_id=79f0eb09798bde55df9e">
         Sign In with GitHub
       </a>
-    </>
-=======
-    <BrowserRouter>
-      <NavBar />
 
       <Switch>
         <Route path="/" exact>
@@ -39,7 +30,6 @@ function App() {
         </Route>
       </Switch>
     </BrowserRouter>
->>>>>>> e56f08ac62caef2ef9b6d843ad5e7c16baf07060
   );
 }
 
